c47d2278b0bd299144dd009faa5700f4bf61841b
Camilapcmoura <assiscamila84@gmail.com>
2023-06-15 23:05:57 -0300

    agora deu certo

--- scripts.js
+++ scripts.js
@@ -1,24 +1,39 @@
-const convertButton = document.querySelector (".convert-button")
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-function convertValues(){
-   const inputCurrencyValue = document.querySelector(".input-currency").value
-   const currencyValuetToConvert = document.querySelector(".currency-value-to-convert")
-   const currencyValueConverted = document.querySelector(".currency-value")
-      
-   const dolarToday= 5.2
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValuetToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
 
-   const convertedValue = inputCurrencyValue / dolarToday
+    console.log(currencySelect.value)
+    const dolarToday = 5.2
+    const euroToday = 6.2
 
-   currencyValuetToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL"
-   }).format(inputCurrencyValue)
+     
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
+            style:"currency",
+            currency:"EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
 
 
-   currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD"
-   }).format(convertedValue)
+    currencyValuetToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+
 
 }
 
