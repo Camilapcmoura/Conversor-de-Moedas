bf3702e2a65fe96a6c7a8e14037514f00268d1d8
Camilapcmoura <assiscamila84@gmail.com>
2023-06-15 22:34:48 -0300

    Até aqui deu certo

--- scripts.js
+++ scripts.js
@@ -1,39 +1,25 @@
-const convertButton = document.querySelector(".convert-button")
-const currencySelect = document.querySelector(".currency-select")
+const convertButton = document.querySelector (".convert-button")
 
-function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
-    const currencyValueConverted = document.querySelector(".currency-value")
+function convertValues(){
+   const inputCurrencyValue = document.querySelector(".input-currency").value
+   const currencyValuetToConvert = document.querySelector(".currency-value-to-convert")
+   const currencyValueConverted = document.querySelector(".currency-value")
+      
+   const dolarToday= 5.2
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
+   const convertedValue = inputCurrencyValue / dolarToday
+
+   currencyValuetToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+   }).format(inputCurrencyValue)
 
 
-    if (currencySelect.Value == "dolar") {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
-        }).format(inputCurrencyValue / dolarToday)
-    }
+   currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+   }).format(convertedValue)
 
-    if (currencySelect.Value == "euro") {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-            style: "currency",
-            currency: "EUR"
-        }).format(inputCurrencyValue / euroToday)
-
-    }
-
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-Br", {
-        style: "currency",
-        currency: "BRL"
-    }).format(inputCurrencyValue)
-
-    function changeCurrency() {
-        console.log("trocou de moeda")
-
-    }
 }
-currencySelect.addEventListener("change", changeCurrency)
+
 convertButton.addEventListener("click", convertValues)
